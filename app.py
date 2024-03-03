0da44f8da94f0ab63c3d85f8498622d407311463
Organ <realorgan@hotmail.com>
2024-03-03 13:26:24 +0100

    added matmo

---
+++ app.py
@@ -0,0 +1,14 @@
+matmo = {
+    "MATH": 4,
+    "PC": 4,
+    "SVT": 4,
+    "AR": 2,
+    "FR": 3,
+    "ENG": 3,
+    "HG": 2,
+    "INFO": 2,
+    "SPORT": 2,
+    "ISLAMICEDU": 2,
+    "PHILO": 2,
+    "BEHAV": 1
+}
